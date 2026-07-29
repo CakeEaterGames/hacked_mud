71eaccd98d1ededfb415b6a402755f19953a8be6
Ruslan Taranov <taranov.rr@iturupbank.ru>
2026-07-29 14:34:26 +1100

    removed extra file

--- backend/src/modules/memreader/parsers/LinkedObject.ts
+++ backend/src/modules/memreader/parsers/LinkedObject.ts
@@ -1,5 +1,4 @@
 import { log } from "@backend/plugins/logger/logger";
-import { bigintToFloat32, bigintToFloat64 } from "./FloatParser";
 import { MemoryReader } from "./MemoryReader";
 import type { MonoParser } from "./MonoParser";
 import type { ModuleInfo } from "./ProcParser";
@@ -406,4 +405,19 @@ export enum TypeCode {
   SENTINEL = 0x41,
   PINNED = 0x45,
   ENUM = 0x55,
+}
+
+export function bigintToFloat64(rawBits: bigint): number {
+  const buffer = new ArrayBuffer(8);
+  const view = new DataView(buffer);
+  view.setUint32(0, Number(rawBits & 0xffffffffn), true);
+  view.setUint32(4, Number((rawBits >> 32n) & 0xffffffffn), true);
+  return view.getFloat64(0, true);
+}
+
+export function bigintToFloat32(rawBits: bigint): number {
+  const buffer = new ArrayBuffer(4);
+  const view = new DataView(buffer);
+  view.setUint32(0, Number(rawBits & 0xffffffffn), true);
+  return view.getFloat32(0, true);
 }
