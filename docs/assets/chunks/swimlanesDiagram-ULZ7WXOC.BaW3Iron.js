ba34896e3857bc730b24410de352ef2182927bca
RuslanTaranov <ruslan.taranov.sakh@gmail.com>
2026-08-09 20:58:50 +1100

    pages v0

---
+++ docs/assets/chunks/swimlanesDiagram-ULZ7WXOC.BaW3Iron.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-UKHOOZJN.S0284ZN5.js";import{_ as a}from"./theme.BJALBVtz.js";import"./chunk-5VM5RSS4.gyes9UNn.js";import"./chunk-XXDRQBXY.CAOa9PwE.js";import"./chunk-KBJHAD2P.DRsVAOXr.js";import"./chunk-2GRJ4B5K.C4s3KJsD.js";import"./framework.8iCprRBG.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
