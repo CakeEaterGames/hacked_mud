612bf2248175c878871838cea21c65fe2df25a57
RuslanTaranov <ruslan.taranov.sakh@gmail.com>
2026-08-10 20:47:36 +1100

    Removed outdated github paged folder

--- docs/assets/chunks/demo.CTFKs-z8.js
+++
@@ -1,1 +0,0 @@
-const s="/hacked_mud/assets/demo.f6dsp0_i.gif";export{s as _};
--- docs/assets/chunks/swimlanesDiagram-G3AALYLV.BeZOmaSK.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-23GEKE2U.C3UUk6cY.js";import{_ as a}from"./theme.DopKK1k6.js";import"./chunk-5VM5RSS4.CB1G3GCY.js";import"./chunk-XXDRQBXY.R8w45occ.js";import"./chunk-VR4S4FIN.D8aRm-lp.js";import"./chunk-32BRIVSS.BHQVthLp.js";import"./framework.CgSZb0NU.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
